ce22b1bc14a333ad7af1964a358e7372cf804a9e
Ucok23 <ulubis98@gmail.com>
2025-10-06 17:39:11 +0700

    disable no-unescaped-entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off'
+    },
+  }),
   {
     ignores: [
       "node_modules/**",
